0199fb2fe34e112fe69e93e9a7e4e9cb96e20832
ryota-arakura <ryota-arakura.19@ezweb.ne.jp>
2021-09-12 14:36:26 +0900

    MyDjangogirls app, force commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Post
 
 # Create your views here.
@@ -6,4 +7,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
